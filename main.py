9262acd0920355bfad94a460090e6c0474265cee
Daniel Bautista <68715227+daniel-baf@users.noreply.github.com>
2023-02-14 03:19:11 -0600

    codigo de sec. 5

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+# import section
+import os
+
+# info section
 print("Usuario: Daniel Eduardo Bautista Fuentes")
 print("Carnet: 2121323")
+
+# code section
+mi_ubicacion = os.getcwd() 
+
+if os.path.exists("modulos"):
+    print("La carpeta existe")
+else:
+    os.mkdir(mi_ubicacion + "/modulos") # cambiando \\ por / por ser linux
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
+
